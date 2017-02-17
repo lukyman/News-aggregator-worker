ccafead95eb70677a8ead2e13f4b2e0443179aff
Abdulmanan <a.manan59@gmail.com>
2017-02-17 19:23:32 +0300

    mmmh

--- model/rssendpoints.js
+++ model/rssendpoints.js
@@ -1,7 +1,7 @@
 module.exports = {
     "local": {
         "standardmedia": "http://www.standardmedia.co.ke/rss/headlines.php",
-        /*"thestar": "http://www.the-star.co.ke/taxonomy/term/all/rss.xml",
+        "thestar": "http://www.the-star.co.ke/taxonomy/term/all/rss.xml",
         "nation": "http://www.nation.co.ke/latestrss.rss",
         "gafla": "http://www.ghafla.co.ke/component/ninjarsssyndicator/?feed_id=1&format=raw",
         "mediamax": "http://www.mediamaxnetwork.co.ke/news/feed/",
@@ -10,22 +10,22 @@ module.exports = {
         "kenya-today": "http://www.kenya-today.com/politics/feed/",
         "citizen-news": "https://citizentv.co.ke/news/feed/",
         "theguardian-kenya":"https://www.theguardian.com/world/kenya/rss"
-	*/
+	
     },
     "international": {
         "bbc": "http://feeds.bbci.co.uk/news/rss.xml",
-      /*  "aljazeera": "http://www.aljazeera.com/xml/rss/all.xml",
+        "aljazeera": "http://www.aljazeera.com/xml/rss/all.xml",
         "skynews": "http://feeds.skynews.com/feeds/rss/world.xml",
         "voa": "http://www.voanews.com/api/z-$otevtiq",
         "allafrica": "http://allafrica.com/tools/headlines/rdf/business/headlines.rdf",
         "enac": "https://www.enca.com/rss/africa",
         "theguardian-africa": "https://www.theguardian.com/world/africa/rss",
         "theguardian-world":"https://www.theguardian.com/world/rss",
-      	*/
+      	
     },
     "sports": {
         "standardmedia": "http://www.standardmedia.co.ke/rss/sports.php",
-        /*"futaa": "http://www.futaa.com/rss/",
+        "futaa": "http://www.futaa.com/rss/",
         "goal": "http://www.goal.com/en-ke/feeds/news?fmt=rss&ICID=HP",
         "uefa1": "http://www.uefa.com/rssfeed/uefachampionsleague/rss.xml",
         "uefa2": "http://www.uefa.com/rssfeed/uefaeuropaleague/rss.xml",
@@ -36,39 +36,39 @@ module.exports = {
         "mchezoafrika": "http://www.michezoafrika.com/rss.aspx",
         "goonerpark": "http://goonerpark.org/feed/",
         "citizen-sports":"https://citizentv.co.ke/sports/feed/",
-*/
+
     },
     "lifestyle": {
 		
         "huffingtonpost": "http://www.huffingtonpost.co.uk/feeds/verticals/uk-style/blog.xml",
-  /*      "justlux": "http://www.justluxe.com/rss/rss-channels.php?sec=fineliving",
+        "justlux": "http://www.justluxe.com/rss/rss-channels.php?sec=fineliving",
         "healthywoman": "http://healthywoman.co.ke/feed/",
         "luciamusau": "http://luciamusau.com/feed/",
         "edaily": "https://edaily.co.ke/feed/",
         "citizen-lifestyle":"https://citizentv.co.ke/lifestyle/feed/"
-*/
+
     },
     "technology": {
         "venturebeat": "http://feeds.feedburner.com/venturebeat/SZYF?format=xml",
-  /*      "forbes": "http://www.forbes.com/technology/feed/",
+        "forbes": "http://www.forbes.com/technology/feed/",
         "thenextweb": "http://thenextweb.com/feed/",
         "itnewsafrica": "http://itnewsafrica.com/?feed=rss2",
         "techweez": "http://feeds.feedburner.com/techweez",
         "droid254":"http://www.droid254.com/feed/",
-        */
+        
     },
     
     "science": {
         "foxscience": "http://feeds.foxnews.com/foxnews/science",
-        /*"scienceblog": "https://scienceblog.com/feed/",
+        "scienceblog": "https://scienceblog.com/feed/",
         "sciencedaily":"https://rss.sciencedaily.com/all.xml"
-        */
+        
     },
     "entertainment": {
         "standardmedia": "http://www.standardmedia.co.ke/rss/entertainment.php",
-        /*"holywoodreporter": "http://feeds.feedburner.com/thr/film",
+        "holywoodreporter": "http://feeds.feedburner.com/thr/film",
         "holywoodreporter1": "http://feeds.feedburner.com/thr/music",
         "holywoodreporter2": "http://feeds.feedburner.com/thr/boxoffice"
-*/
+
     }
 }
