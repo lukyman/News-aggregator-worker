80a2740d527a6aa697dd96ed6740866582ef556e
Abdulmanan <a.manan59@gmail.com>
2017-02-17 08:01:06 +0300

    port change

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
+var http = require('http');
 var Jobs = require('./jobs/agendaConfig');
 var feedssort = require('./jobs/sortfeeds');
-const http = require('http');
-const port = 3001;
-
-
 Jobs.setupJobs();
-
 http.createServer(function (req, res) {
     
-}).listen(port);
+}).listen(3000,'127.0.0.1');
 
